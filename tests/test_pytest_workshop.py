05207c2f349d41c8fbadda64ac2012cf9d4d6cc6
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-06-27 07:38:34 +0100

    Removed all demo code

--- tests/test_pytest_workshop.py
+++ tests/test_pytest_workshop.py
@@ -8,23 +8,3 @@ test_pytest_workshop
 Tests for `pytest_workshop` module.
 """
 
-import pytest
-
-
-from pytest_workshop import pytest_workshop
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
