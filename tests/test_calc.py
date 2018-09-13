718185bbe779d3e58f969aa88400a52504b3e126
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-09-13 17:25:44 +0100

    T add(): add two numbers

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -7,4 +7,12 @@ test_pytest_workshop
 
 Tests for `pytest_workshop` module.
 """
+from pytest_workshop.calc import Calc
 
+
+def test_add_two_numbers():
+    c = Calc()
+
+    res = c.add(4, 5)
+
+    assert res == 9
