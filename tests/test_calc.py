b87b1fc114337393339d867c56103bae8cec24a5
Leonardo Giordani <giordani.leonardo@gmail.com>
2018-09-13 16:53:22 +0100

    T avg(): remove lower outliers

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -67,3 +67,7 @@ def test_avg_correct_average():
 
 def test_avg_removes_upper_outliers():
     assert Calc().avg([2, 5, 12, 98], ut=90) == pytest.approx(6.333333)
+
+
+def test_avg_removes_lower_outliers():
+    assert Calc().avg([2, 5, 12, 98], lt=10) == 55
